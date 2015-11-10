4e3be8dcb3cb39afdc49e0fcc4b317793c643eb7
Andy Gregorowicz <andy@gregorowicz.org>
2015-11-09 21:23:11 -0500

    Initial commit

---
+++ app/models/patient.rb
@@ -0,0 +1,12 @@
+class Patient < ActiveRecord::Base
+  extend Enumerize
+
+  validates :first_name, :last_name, :birthdate, :gender, presence: true
+  enumerize :gender, in: [:male, :female]
+
+  after_initialize do
+    if new_record?
+      self.birthdate ||= Date.new(1955, 11, 11)
+    end
+  end
+end
---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,43 @@
+RailsAdmin.config do |config|
+
+  ### Popular gems integration
+
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  ## == Cancan ==
+  # config.authorize_with :cancan
+
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+
+  config.model 'Patient' do
+    list do
+      field :first_name
+      field :last_name
+      field :birthdate
+      field :gender
+    end
+  end
+end
